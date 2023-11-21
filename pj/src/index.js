91c4d17e1047749c231b93622a766fdc7d230e9f
ckdwns1221 <ckdwns2181@gmail.com>
2023-11-21 13:22:24 +0900

    경로 수정

--- pj/src/index.js
+++ pj/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+/* import App from './App'; */
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter} from 'react-router-dom';
+import Login from './pages/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Login />
     </BrowserRouter>
   </React.StrictMode>
 );
