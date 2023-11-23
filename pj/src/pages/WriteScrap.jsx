33059d36dee4aa1ba1d7184c32904cfe349b19a8
ckdwns1221 <ckdwns2181@gmail.com>
2023-11-24 01:52:15 +0900

    채원 업로드 사항 합침

--- pj/src/pages/WriteScrap.jsx
+++ pj/src/pages/WriteScrap.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React/* , { useState, useEffect } */ from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import '../css/WriteScrap.css'
+import Navbar from '../components/Navbar'
+
 
 export default function WriteScrap() {
+
+
+    const movePage= useNavigate();
+
+    function moveMain() {
+      movePage('/');
+    }
     return (
         <div className='display-container'>
+            <Navbar />
+            <div className="wTitleWrap">
+                    스크랩 추가하기
+            </div>
+            <div className='WriteScrap-container'>
+                <div className="wContentWrap">
+                    <div className='wInputTitle'>
+                        제목
+                    </div>
+                    <div className='wInputWrap'>
+                        <input 
+                            type='text'
+                            className='wInput' 
+                            placeholder='스크린 제목 입력해주세요.' />
+                    </div>
+                    <div className="wInputTitle" style={{ marginTop: "26px"}}>
+                        파일 첨부
+                    </div>
+                    <div className="wInputWrap">
+                        <input 
+                            type="file" 
+                            className='wInputFile'
+                            accept=".jpg, .jpeg, .png, .gif, .ppt, .docs, .hwp, .pdf, .xlsx" 
+                            placeholder='또는 파일을 여기로 드래그 해주세요.'
+                            multiple />
+                    </div>
+                    <div className='wInputTitle' style={{ marginTop: "26px"}}>
+                        링크 첨부
+                    </div> 
+                    <div className='wInputWrap'>
+                        <input 
+                            type='text'
+                            className='wInput' 
+                            placeholder='링크를 붙여 넣어주세요.' />
+                    </div>
+                    <div className='wInputTitle' style={{ marginTop: "26px"}}>
+                        메모 작성
+                    </div> 
+                    <div className='wInputWrap'>
+                        <input 
+                            type='text'
+                            className='wInput' 
+                            placeholder='스크랩에 대해 설명해주세요.' />
+                    </div>
+                    <div className='wInputTitle' style={{ marginTop: "26px"}}>
+                        카테고리
+                    </div> 
+                    <div className="categoryBtn">
+                        <button className="cBtnNews">시사/뉴스</button>
+                        <button className="cBtnFood">푸드</button>
+                        <button className="cBtnArts">문화/예술</button>
+                        <button className="cBtnFinance">경제/금융</button>
+                        <button className="cBtnTech">IT/기술</button>
+                        <button className="cBtnHealth">건강/의학</button>
+                        <button className="cBtnBusiness">비즈니스</button>
+                        <button className="cBtnEtc">기타</button>
+                    </div>
+                    <div className='wInputTitle' style={{ marginTop: "26px"}}>
+                        공개 여부   
+                    </div> 
+                    <div className="releaseBtn">
 
+                    </div>
+                </div>
+                <button className='wBottomBtn'  onClick={moveMain}>
+                    스크랩 등록
+                </button>
+            </div>
         </div>
     )
 }
