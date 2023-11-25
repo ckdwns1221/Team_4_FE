ebac819063c08e4570a72e9ee5ce3360e80c330b
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2023-11-25 10:18:05 +0900

    feat:login 회원 기능

--- pj/src/apis/config.js
+++ pj/src/apis/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080/api/';
+axios.defaults.baseURL = 'https://www.twoneone.store/api/';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
